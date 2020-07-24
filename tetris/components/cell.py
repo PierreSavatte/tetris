78622ebee2f4f5b3b1821b5b046508308511a385
PierreSavatte <pierre-savatte@laposte.net>
2020-07-24 23:26:40 +0200

    Correct test if cell.future_position in position_of_deactivated_cells

--- tetris/components/cell.py
+++ tetris/components/cell.py
@@ -14,7 +14,7 @@ class Cell(Drawable):
         future_position = (x, y + 1)
         if not board.position_is_inside(future_position):
             raise CanNotMoveDown("Can not go outside the board")
-        elif future_position not in board.get_positions_of_deactivated_cells():
+        elif future_position in board.get_positions_of_deactivated_cells():
             raise CanNotMoveDown("Can not override a deactivated cell")
         else:
             self.position = future_position
